9fbb5e775c2f9330dbf15d05f764f59cb56c4502
PRAKAMYASINGH <75196173+PRAKAMYASINGH@users.noreply.github.com>
2021-04-04 12:24:06 +0530

    first commit

---
+++ MatterJSBoilerPlate-master/ground.js
@@ -0,0 +1,33 @@
+class ground
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+
+    // the display function to display in sketch.js ...
+	display()
+	{
+			
+			var groundPosition=this.body.position;		
+
+			push()
+			translate(groundPosition.x, groundPosition.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			fill(255,255,0)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
